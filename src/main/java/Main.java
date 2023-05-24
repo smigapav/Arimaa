ccf770c5eee7c9ffbc79cbfb0679ebe8e34ab76a
Pavel Smiga <smigapav@fel.cvut.cz>
2023-05-24 12:47:00 +0200

    Added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import cz.cvut.fel.pjv.arimaa.model.Board;
+import cz.cvut.fel.pjv.arimaa.view.StartMenu;
+import cz.cvut.fel.pjv.arimaa.view.Table;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StartMenu menu = new StartMenu();
+
+//        Board board = new Board(true, true);
+
+//        for (int i = 0; i < 8; i++) {
+//            board.getGoldPlayer().placeFigure(1,i);
+//        }
+//        for (int i = 0; i < 8; i++) {
+//            board.getGoldPlayer().placeFigure(0,i);
+//        }
+//
+//        board.changeCurrentPlayer();
+//
+//        for (int i = 0; i < 8; i++) {
+//            board.getSilverPlayer().placeFigure(6,i);
+//        }
+//        for (int i = 0; i < 8; i++) {
+//            board.getSilverPlayer().placeFigure(7,i);
+//        }
+//
+//        board.changeCurrentPlayer();
+
+//        Table table = new Table(board);
+
+    }
+}
